79a2deaa859362c1b8bbb1e63467599968fb975f
hanzaa <farhan.2523@gmail.com>
2023-12-30 01:16:18 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { notificationProvider, RefineThemes } from "@refinedev/chakra-ui";
+import {ErrorComponent, ThemedLayoutV2, notificationProvider, RefineThemes } from "@refinedev/chakra-ui";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import routerBindings, {
+  NavigateToResource,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter,Outlet, Route, Routes } from "react-router-dom";
+import { ChakraUIInferencer } from "@refinedev/inferencer/chakra-ui";
 
 function App() {
   return (
@@ -24,6 +26,15 @@ function App() {
               notificationProvider={notificationProvider}
               routerProvider={routerBindings}
               dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+              resources={[
+                {
+                  name: "blog_posts",
+                  list: "/blog-posts",
+                  show: "/blog-posts/show/:id",
+                  create: "/blog-posts/create",
+                  edit: "/blog-posts/edit/:id",
+                },
+              ]}
               options={{
                 syncWithLocation: true,
                 warnWhenUnsavedChanges: true,
@@ -31,9 +42,24 @@ function App() {
                 projectId: "4rspVd-DYEvH9-LqSqRD",
               }}
             >
-              <Routes>
-                <Route index element={<WelcomePage />} />
-              </Routes>
+          <Routes>
+            <Route
+              element={
+                <ThemedLayoutV2>
+                  <Outlet />
+                </ThemedLayoutV2>
+              }
+            >
+              <Route index element={<NavigateToResource resource="blog_posts" />} />
+              <Route path="blog-posts">
+                <Route index element={<ChakraUIInferencer />} />
+                <Route path="show/:id" element={<ChakraUIInferencer />} />
+                <Route path="edit/:id" element={<ChakraUIInferencer />} />
+                <Route path="create" element={<ChakraUIInferencer />} />
+              </Route>
+              <Route path="*" element={<ErrorComponent />} />
+            </Route>
+          </Routes>
               <RefineKbar />
               <UnsavedChangesNotifier />
               <DocumentTitleHandler />
